02ac84790bbcef361b2dae3abd7220d85f2817de
MuhammadAbdulhamidDev <muhammad.abdulhamid.dev@gmail.com>
2024-01-29 12:55:49 +0200

    preloader commit

--- js/modules/preloader.module.js
+++ js/modules/preloader.module.js
@@ -1,7 +1,15 @@
 function outerLoader() {
-  $(function () {
-    $(".outer-spin").fadeOut(1000, function () {
-      $(".loading-screen").slideUp(500);
+  // $(function () {
+  //   $(".outer-spin").fadeOut(1000, function () {
+  //     $(".loading-screen").slideUp(500);
+  //     $("body").css({ overflow: "auto" });
+  //     $("body").animate({ scrollTop: 0 }, 0);
+  //   });
+  // });
+
+  $(document).ready(function () {
+    $(window).on("load", function () {
+      $(".loading-screen").fadeOut("slow");
       $("body").css({ overflow: "auto" });
       $("body").animate({ scrollTop: 0 }, 0);
     });
@@ -9,11 +17,20 @@ function outerLoader() {
 }
 
 function innerLoader() {
-  $(function () {
-    $("body").css({ overflow: "auto" });
-    $("body").animate({ scrollTop: 0 }, 0);
-    $(".inner-loading-screen").fadeIn(500, function () {
-      $(".inner-loading-screen").fadeOut(250);
+  // $(function () {
+  //   $("body").css({ overflow: "auto" });
+  //   $("body").animate({ scrollTop: 0 }, 0);
+  //   $(".inner-loading-screen").fadeIn(500, function () {
+  //     $(".inner-loading-screen").fadeOut(250);
+  //   });
+  // });
+  $(document).ready(function () {
+    $(window).on("load", function () {
+      $(".inner-loading-screen").fadeIn(500, function () {
+        $("body").css({ overflow: "auto" });
+        $("body").animate({ scrollTop: 0 }, 0);
+        $(".inner-loading-screen").fadeOut(250);
+      });
     });
   });
 }
